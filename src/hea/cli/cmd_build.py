3f878e4d4d6f725dda6a9702af38995521d1d3b3
Juraj Ovčar <juraj.ovcar@gmail.com>
2025-04-14 10:28:46 +0200

    CLI: Add build and check commands
    Add "hea build" and "hea check" commands for building HEA structures and
    checking the obtained concentrations.

--- src/hea/cli/cmd_build.py
+++ src/hea/cli/cmd_build.py
@@ -5,6 +5,9 @@ import os
 import click
 
 from hea.builder.build_config import BuildConfig
+from hea.builder.builder import Builder
+
+from hea.builder import analyze_build
 
 @click.group('build', invoke_without_command=True,
     help="""*** Build HEA structures ***
@@ -19,21 +22,37 @@ from hea.builder.build_config import BuildConfig
 @click.option('-c', '--config-file', 'config_file',
               type=click.Path(dir_okay=False),
               help='Path to the TOML build configuration file')
+@click.option('-o', '--output-directory', 'output_directory',
+              type=click.Path(file_okay=False),
+              required=False,
+              help='Path to output build directory [default: CWD]',
+              default=os.getcwd())
+@click.option('-p', '--prefix', 'prefix',
+              required=False,
+              help='Prefix of built files',
+              default='HEA',
+              show_default=True)
 @click.pass_context
-def build_cli(ctx, config_file):
+def build_cli(ctx, config_file, output_directory, prefix):
     """
     Build CLI
 
     """
 
     if ctx.invoked_subcommand is None:
-        click.echo('hea build <build configuration file> not yet implemented.')
+
+        if not config_file:
+            click.echo(ctx.get_help())
+            return
+
+        builder = Builder(config_filepath=config_file)
+        builder.build(build_directory=output_directory, prefix=prefix)
 
 @build_cli.command('init',
     help="""Create a default build configuration file in the CWD
 
     Usage:
-        
+
         hea build init
 
     """)
@@ -52,3 +71,22 @@ def init(filename):
 
     build = BuildConfig()
     build.write_defaults(output_filepath=path)
+
+@click.argument('filename', required=True)
+@build_cli.command('check',
+    help="""Check concentrations in a TRAJ file
+
+    Usage:
+
+        hea build check HEA.traj
+
+    """)
+def check(filename):
+    """
+    Check concentrations in a TRAJ file
+
+    """
+
+    chemistry = analyze_build.get_concentrations(traj_filepath=filename)
+
+    click.echo(analyze_build.report_chemistry(chemistry=chemistry))
